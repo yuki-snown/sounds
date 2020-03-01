6f7ac092532a51fc341a3e5b5e2b6174e75576b4
yuki-snown <yuki.snowman.0103@icloud.com>
2020-03-01 15:35:49 +0900

    gemfile pg 修正

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'sqlite3', '~> 1.4'
+
 end
 
 group :development do
@@ -47,7 +48,7 @@ group :test do
 end
 
 group :production do
-  gem 'pg', '0.20.0'
+  gem 'pg', '1.1.4'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/routes.rb
+++ config/routes.rb
@@ -11,21 +11,21 @@ Rails.application.routes.draw do
   get "favorites" => "favorites#index"
 
   #音楽登録API
-  post "resister" => "contents#resister" #OK
+  post "resister" => "contents#resister"
   #お気に入り登録
-  post "fav/add" => "favorites#add" #OK
+  post "fav/add" => "favorites#add"
 
   #自分が投稿した音楽
-  get "post" => "users#home" #OK
-  get "home" => "contents#home" #OK
+  get "post" => "users#home"
+  get "home" => "contents#home"
   #タイトル検索
-  get "search" => "contents#search" #OK
+  get "search" => "contents#search"
   #直近のN件を取得
-  get "current" => "contents#current" #OK
+  get "current" => "contents#current"
   #閲覧する際のデータ取得
-  get "show" => "contents#watch" #OK
+  get "show" => "contents#watch"
   #お気に入り呼び出し
-  get "fav/search" => "favorites#search" # OK
-  get "fav/show" => "contents#fav" #OK
+  get "fav/search" => "favorites#search"
+  get "fav/show" => "contents#fav"
 
 end
